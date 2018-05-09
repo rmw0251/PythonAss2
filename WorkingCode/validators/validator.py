19f4a0d3edc0936f7a751393e97bf35832a7e77f
rmw0251 <30913291+rmw0251@users.noreply.github.com>
2018-05-09 12:11:17 +1200

    Add files via upload
    adding imports needed to reduce duplicated code

--- WorkingCode/validators/validator.py
+++ WorkingCode/validators/validator.py
@@ -1,4 +1,17 @@
 import re
+import sys
+
+try:
+    from errors import ErrorHandler as Err
+except NameError and ModuleNotFoundError and ImportError:
+    print("Fatal Error - Errors.py not found.")
+    sys.exit()
+
+try:
+    from washers.washer import Washer as Wa
+except NameError and ModuleNotFoundError and ImportError:
+    print(Err.get_error_message(404, "washer.py"))
+    sys.exit()
 
 
 class Validator(object):
